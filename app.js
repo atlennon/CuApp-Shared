b7110beeae37363f4980c20d64b1f5c9975f4bde
atlennon <atlennon@gmail.com>
2014-02-18 15:01:31 -0600

    Changed view engine to handlebars express

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
+var express = require('express'),
+    mysql = require('mysql'),
+	exphbs  = require('express3-handlebars'),
+	path = require('path'),
+    app = express();
+	
+// DB connection setup
+ 
+var connection = mysql.createConnection({
+        host:'localhost',
+        user:'uVY6VvL4pXhkS',
+        password:'pvpUV3ZnkZGJ8',
+		port:10000
+    });	
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-//var routes = require('./routes');
-//var user = require('./routes/user');
-//var http = require('http');
-var path = require('path');
-
-var app = express();
-
-// all environments
+//All environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -23,16 +26,9 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-//app.get('/', routes.index);
-//app.get('/users', user.list);
-require('./routes')(app);
-//http.createServer(app).listen(app.get('port'), function(){
-//  console.log('Express server listening on port ' + app.get('port'));
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
 app.listen(app.get('port'), function(){
 console.log('Express server listening on port ' + app.get('port'));
--- controllers/staticController.js
+++ controllers/staticController.js
@@ -1,6 +1,9 @@
+exports.home = function(req, res){
+  res.render('home.handlebars', { title: 'Credit Union App' });
+};
 exports.index = function(req, res){
-  res.render('index', { title: 'Credit Union App' });
+  res.render('index.html', { title: 'Credit Union App' });
 };
 exports.appform = function(req, res){
-  res.render('form.html', { title: 'Application form' });
+  res.render('appform', { title: 'Application form' });
 };
